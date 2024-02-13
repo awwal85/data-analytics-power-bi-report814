0ffe1022d0469513c7a575c35034cee471d45248
awwal85 <114372396+awwal85@users.noreply.github.com>
2024-02-13 00:36:10 +0000

    Add files via upload

---
+++ Question_3.sql
@@ -0,0 +1,21 @@
+SELECT 
+    orders.store_code::text, 
+    orders.total_orders,
+    country_region.country::text,
+    dim_date.year 
+
+FROM 
+    orders,
+    country_region,
+    dim_date
+
+WHERE
+    dim_date.year::text LIKE '%22' AND country::text LIKE 'Ge%'
+
+GROUP BY 
+    country::text, store_code::text, total_orders, dim_date.year
+
+ORDER BY
+    country::text, total_orders DESC
+
+LIMIT 1; 
